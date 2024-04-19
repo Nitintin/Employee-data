16e8581591b811160f97834add837175bc7f73e0
nitin gupta <ngupta5@expediagroup.com>
2024-04-19 10:12:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const {Employee} =  require('./Data/Employee.js');
+
+console.log('Employee', Employee);
+const PORT = 5001;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.get('/employees', (req, res) => {
+    res.send(Employee);
+});
+
+app.get('/portraits/:portraitName', (req, res) => {
+    const { portraitName } = req.params;
+    const portraitPath = `./portraits/${portraitName}`;
+    res.sendFile(portraitPath, { root: __dirname });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
